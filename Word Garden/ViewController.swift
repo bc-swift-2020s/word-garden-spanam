bd8bbff7518a651310bdd204018d173f841f2dae
Matthew Spana <spanam@bc.edu>
2020-02-05 00:24:07 -0500

    Section 3.2 - Added primary layout and basic functionality to letter input.

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
+    var guesses = 0;
+    
+    func setNumberGuesses() {
+        guessCountLabel.text = "You've made \(guesses) Guesses"
+    }
+    
+    func resetGuessedLetterField() {
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setNumberGuesses()
     }
 
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        resetGuessedLetterField()
+    }
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        if guessedLetterField.text == nil {
+            
+        }
+    }
+    
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        resetGuessedLetterField()
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
